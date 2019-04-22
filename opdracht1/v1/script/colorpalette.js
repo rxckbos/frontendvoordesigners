1590d974837b43a1c7b2f510044c5c9ee7a89fb0
Rick <rxckbos@gmail.com>
2019-04-23 00:51:56 +0200

    color filter update

--- opdracht1/v1/script/colorpalette.js
+++ opdracht1/v1/script/colorpalette.js
@@ -1,18 +1,5 @@
-//*Foto blue
-/*
-var buttonblue = document.getElementsByClassName('.blue_button');
-var imagered = document.getElementsByClassName('.red-image');
-
-function startbluefilter() {
-      imagered.classList.toggle('.red-image-invisible');
-}
-
-buttonblue.addEventListener('click', startbluefilter);
-*/
-
-
-
 //BUTTONBLUE
+/*
 var buttonblue = document.querySelector('.buttonblue');
 var imagered = document.querySelector('.imagered');
 
@@ -22,6 +9,23 @@ function startbutton() {
 
 function hideimagered() {
   imagered.classList.toggle('hideimagered');
+}
+
+buttonblue.addEventListener('click', startbutton);
+buttonblue.addEventListener('click', hideimagered);
+*/
+
+var buttonblue = document.querySelector('.buttonblue');
+var imagered = document.querySelector('.imagered');
+var imageAll = document.querySelector('img');
+
+function startbutton() {
+  buttonblue.classList.toggle('startbutton');
+}
+
+function hideimagered() {  
+    imageAll.classList.toggle('hideallimages');
+  /*imagered.classList.add('hideimagered');*/
 }
 
 buttonblue.addEventListener('click', startbutton);
