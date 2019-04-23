b11560eb9c03f0e92fd151e7133d6647dfe816e0
Rick <rxckbos@gmail.com>
2019-04-23 11:46:05 +0200

    Yellow button JS code

--- opdracht1/v1/script/colorpalette.js
+++ opdracht1/v1/script/colorpalette.js
@@ -1,13 +1,24 @@
-//BUTTONBLUE
+//Filter on blue images.
+//Variables for al images and buttons
 var buttonblue = document.querySelector('.buttonblue');
+var buttonyellow = document.querySelector('.buttonyellow');
+
+var imageblue = document.querySelectorAll('.imageblue');
 var imageyellow = document.querySelectorAll('.imageyellow');
 var imagered = document.querySelectorAll('.imagered');
 var imagegreen = document.querySelectorAll('.imagegreen');
 
-function startbutton() {
+//Select button blue
+function buttonbluetransform() {
   buttonblue.classList.toggle('startbutton');
 }
 
+//Select button yellow
+function buttonyellowtransform() {
+    buttonyellow.classlist.toggle('startbutton');
+}
+
+//Hide all images with other color
 function hideForBlue() {
     for(var hideimageyellow of imageyellow){
         console.log(hideimageyellow);
@@ -25,5 +36,12 @@ function hideForBlue() {
     }
 }
 
-buttonblue.addEventListener('click', startbutton);
+//Blue button
+buttonblue.addEventListener('click', buttonbluetransform);
 buttonblue.addEventListener('click', hideForBlue);
+
+//Yellow Button
+buttonyellow.addEventListener('click', buttonyellowtransform);
+
+
+
