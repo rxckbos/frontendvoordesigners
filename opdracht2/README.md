d134b7ea337a20143fa70ace8c6b53726b7fc8b4
Rick <rxckbos@gmail.com>
2019-05-21 00:51:08 +0200

    Progress

--- opdracht2/README.md
+++ opdracht2/README.md
@@ -1,3 +1,19 @@
 # frontend voor designers - opdracht 2
+Als ik foto's zoek wil ik kunnen selecteren op kleur om verassende zoekresultaten te krijgen. Door op een van de kleuren te klikken boven aan de pagina, verschijnen de afbeeldinge met deze kleur.
 
-Tekst voor opdracht 2.
+#### Link naar demo:
+
+#### Link naar de broncode:
+
+### 06. One primary action per screen
+Explaination
+
+### 08. Provide a natural next step
+Explaination
+
+### 14. Progressive disclosure
+Explaination
+
+## Tweaks V2:
+
+### Futere tweaks:
---
+++ opdracht2/v1/script/script.js
@@ -0,0 +1,98 @@
+var numSteps = 20.0;
+
+var boxElement;
+var prevRatio = 0.0;
+var increasingColor = "rgba(00, 00, 00, ratio)";
+var decreasingColor = "rgba(ff, ff, ff, ratio)";
+
+// Set things up.
+
+window.addEventListener("load", function (event) {
+    boxElement = document.querySelector(".section-1");
+
+    createObserver();
+}, false);
+
+function createObserver() {
+    var observer;
+
+    var options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: buildThresholdList()
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(boxElement);
+}
+
+function buildThresholdList() {
+    var thresholds = [];
+    var numSteps = 20;
+
+    for (var i = 1.0; i <= numSteps; i++) {
+        var ratio = i / numSteps;
+        thresholds.push(ratio);
+    }
+
+    thresholds.push(0);
+    return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > prevRatio) {
+            entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
+        } else {
+            entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
+        }
+
+        prevRatio = entry.intersectionRatio;
+    });
+}
+
+//Section 2
+
+window.addEventListener("load", function (event) {
+    boxElement = document.querySelector(".section-2");
+
+    createObserver();
+}, false);
+
+function createObserver() {
+    var observer;
+
+    var options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: buildThresholdList()
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(boxElement);
+}
+
+function buildThresholdList() {
+    var thresholds = [];
+    var numSteps = 20;
+
+    for (var i = 1.0; i <= numSteps; i++) {
+        var ratio = i / numSteps;
+        thresholds.push(ratio);
+    }
+
+    thresholds.push(0);
+    return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > prevRatio) {
+            entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
+        } else {
+            entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
+        }
+
+        prevRatio = entry.intersectionRatio;
+    });
+}
